0308342b806683bbe950fbc4bb74a631b61951ea
Baitianliang <949591088@qq.com>
2025-07-08 09:35:51 +0800

    删除没用的反向代理地址

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        // target: 'http://jsonplaceholder.typicode.com',
         target: 'https://dls.4dlp.com.cn:7102/dls-strengthen/',
-        // target: 'http://192.168.2.173:8092/dls-strengthen/',
-        // target: 'http://10.73.60.92:8092',
         changeOrigin: true,
         pathRewrite: {
             '^/api': '',
